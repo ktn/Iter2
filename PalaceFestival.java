48b25988cc2c7d284332cacdb044dd729c869ec1
AlexBujduveanu <alexbujduveanu@gmail.com>
2014-03-14 19:54:05 -0400

    PalaceFestival
    Don’t really know how its going to work until we get PlayerFacade set up

---
+++ PalaceFestival.java
@@ -0,0 +1,18 @@
+public class PalaceFestival
+{
+	public PlayerFacade playerFacade;
+
+	public PalaceFestival()
+	{
+		
+	}
+	public void startFestival()
+	{
+		
+	}
+
+	public void endFestival()
+	{
+		//Score fame points
+	}
+}
