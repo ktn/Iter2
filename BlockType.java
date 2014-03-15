3c6b205fccf26cc12d19a1f300fd93e55f202d04
unknown <jonathan.howe.yu@gmail.com>
2014-03-15 17:16:46 -0400

    fixed blocktype

--- BlockType.java
+++ BlockType.java
@@ -1,5 +1,5 @@
 public enum BlockType {
-	VILLAGE("Village"), RICE("Rice"), IRRIGATION("Irrigation"), PALACE("Palace"), TWO("Two-space"), THREE("Three-space"),
+	VILLAGE("Village"), RICE("Rice"), IRRIGATION("Irrigation"), PALACE("Palace"), TWO("Two-space"), THREE("Three-space"), EMPTY("Empty"),
 	PALACE2("Level 2 Palace"), PALACE4("Level 4 Palace"), PALACE6("Level 6 Palace"), PALACE8("Level 8 Palace"), PALACE10("Level 10 Palace");
 	
 	String s;
