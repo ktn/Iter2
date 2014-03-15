fb516b36391eae1468fb1cbc20fcb0e1f4826f6a
Christopher Waugh <kenkron@gmail.com>
2014-03-14 22:23:18 -0400

    adding view package and some images.

---
+++ view/ViewTester.java
@@ -0,0 +1,16 @@
+package view;
+
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+public class ViewTester {
+	
+	public static void main(String[] args) throws IOException {
+		BoardView boardView=new BoardView();
+		
+		JFrame mainFrame=new JFrame("Java");
+		mainFrame.add(boardView);
+		mainFrame.setVisible(true);
+	}
+}
