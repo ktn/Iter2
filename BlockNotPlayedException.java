309bfe879ec8e7e33db25a0fb64ccceb93b407ad
unknown <jonathan.howe.yu@gmail.com>
2014-03-15 21:08:29 -0400

    Added new exceptions

---
+++ BlockNotPlayedException.java
@@ -0,0 +1,13 @@
+
+public class BlockNotPlayedException extends Exception {
+
+	public BlockNotPlayedException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BlockNotPlayedException(String m) {
+		super(m);
+		// TODO Auto-generated constructor stub
+	}
+
+}
