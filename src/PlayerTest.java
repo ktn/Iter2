4f3b58dc31b5d71f72c5d255793e441ede9a18bb
lucasarango <aranlucas@gmail.com>
2014-03-15 21:00:09 -0400

    Commit

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -1,4 +1,4 @@
-public class playerTest {
+public class PlayerTest {
 
 	public static void main(String[] args) {
 		Deck b = new Deck();
@@ -6,12 +6,20 @@ public class playerTest {
 		String names[] = { "Lucas", "Xy", "Trouble" };
 
 		PlayerFacade p = new PlayerFacade(names);
+
 		System.out.print(b.toString());
 
 		p.addCard(b.drawCard());
+		p.addScore(100);
+		p.addScore(10);
+
+		p.useActionToken();
+
 		System.out.print(p.getCurrentPlayer());
 
 		p.changeTurn();
+		p.addScore(10);
+
 		System.out.print(p.getCurrentPlayer());
 
 	}
