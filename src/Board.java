ea1e433f429273810b795eb91f8c9a7e13a1b3d7
ktn <kevinnieman@gmail.com>
2014-03-15 21:48:31 -0400

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,78 +1,129 @@
-
 public class Board {
 	private Space head;
 	private int xDim = 10;
 	private int yDim = 10;
-	
+
 	/*
 		Head is in the top left corner just fyi
 	*/
-	
-	
+
+	//CONSTRUCTOR  =======================================================================
+
 	Board(){
-		Space[][] temp = new Space[10][10];
+		Space[][] temp = new Space[xDim][yDim];
+		for(int i = 0; i < xDim; i++)
+		{
+			for(int j = 0; j < yDim; j++)
+			{
+				temp[i][j] = new Space();
+			}
+		}
+
 		head = temp[0][0];
-		
+
 		for(int x = 0; x < xDim; x++)
 		{
 			for(int y = 0; y < yDim; y++)
 			{
 				if(x != 0){
-					temp[x][y].join(temp[x-1][y]);
+					//Left
+					temp[x][y].join(2 , temp[x-1][y]);
 				}
-				
+
 				if(y != 0){
-					temp[x][y].join(temp[x][y-1]);
+					//Bottom
+					temp[x][y].join(1 , temp[x][y-1]);
 				}
-				
+
 				if(x != xDim - 1){
-					temp[x][y].join(temp[x+1][y]);
+					//Right
+					temp[x][y].join(0 , temp[x+1][y]);
 				}
-				
-				if(y != xDim - 1){
-					temp[x][y].join(temp[x][y+1]);
+
+				if(y != yDim - 1){
+					//Top
+					temp[x][y].join(3 , temp[x][y+1]);
 				}
-				
+
 			}
 		}
 	}
-	
-	private Space get(int x, int y){
-		Space temp = null;
-		
-		if(x < xDim){
-			if(y < yDim){
-				for(int i = 0; i < x; i++){
+
+	//ACCESSORS  =======================================================================
+	//All methods from here down assume that inbounds indices are passed
+	private Space get(Coordinates c){
+		Space temp = head;
+
+		if(c.x < xDim){
+			if(c.y < yDim){
+				for(int i = 0; i < c.x; i++){
 					temp = temp.getRight();
 				}
-				for(int j = 0; j < y; j++){
+				for(int j = 0; j < c.y; j++){
 					temp = temp.getBottom();
 				}
 			}
 		}
-		
-		
+
 		return temp;
-		
+
 	}
 
-	public Tile getTile(int x, int y){
-		return this.get(x,y).getTile();
+	public int getHeight(Coordinates c){
+		return this.get(c).getHeight();
 	}
-	
-	public void placeBlock(Block b, int x, int y){
-		Space target = this.get(x,y);
+
+	public Tile getTile(Coordinates c){
+		return this.get(c).getTile();
+	}
+
+	public TileType getTileType(Coordinates c){
+		Space temp = this.get(c);
+
+		if(temp.getHeight() == 0)
+			return temp.getTile().getType();
+		else
+			return TileType.EMPTY;
+	}
+
+	public Coordinates getLargest(){
+		return new Coordinates(xDim-1, yDim-1);
+	}
+
+	//BLOCK METHODS  =======================================================================
+
+	public void placeBlock(Block b, Coordinates c){
+		Space target = this.get(c);
 		target.placeTile(b.getTile());
-		
+
 	}
 
-	public void removeBlock(int x, int y){
-		Space target = this.get(x,y);
+	public void removeBlock(Coordinates c){
+		Space target = this.get(c);
 		target.removeTile();
 	}
-	
-	
-	public int getHeight(int x, int y){
-		return this.get(x, y).getHeight();
+
+
+	//CHECKING METHODS  =======================================================================
+	public boolean inBounds(int x, int y){
+		return x <= xDim || y <= yDim;
 	}
+
+	public boolean inBounds(Coordinates c){
+		return c.x <= xDim || c.y <= yDim;
+	}
+
+	public class Coordinates{
+		public int x;
+		public int y;
+
+		Coordinates(int x , int y){
+			if(!inBounds(x, y))
+				throw new IllegalArgumentException("Out of Bounds arguements");
+
+			this.x = x;
+			this.y = y;
+		}
+	}
+
 }
