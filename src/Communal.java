9a3cb5f718770869f24c2babec02ca4df94ae609
lucasarango <lucasarango@ufl.edu>
2014-03-18 00:51:06 -0400

    Update Communal.java

--- src/Communal.java
+++ src/Communal.java
@@ -1,19 +1,20 @@
-import Java.util.ArrayList;
+import java.util.*;
 
-public class Communal
-{
+public class Communal {
 	ArrayList<ThreeBlock> threes;
 	ArrayList<OneBlock> irrigations;
 	ArrayList<Integer> palaceTiles;
-	
-	public Communal()
-	{
+
+	public Communal() {
 		threes = new ArrayList<ThreeBlock>();
-		for(int i = 0; i < 56; i++)
+		irrigations = new ArrayList<OneBlock>();
+		palaceTiles = new ArrayList<Integer>();
+
+		for (int i = 0; i < 56; i++)
 			threes.add(new ThreeBlock());
 		irrigations = new ArrayList<OneBlock>();
-		for(int i = 0; i < 16; i++)
-			threes.add(new OneBlock(TileType.IRRIGATION));
+		for (int i = 0; i < 16; i++)
+			irrigations.add(new OneBlock(TileType.IRRIGATION));
 		palaceTiles = new ArrayList<Integer>();
 		palaceTiles.add(0);
 		palaceTiles.add(6);
@@ -26,61 +27,67 @@ public class Communal
 		palaceTiles.add(0);
 		palaceTiles.add(10);
 	}
-	public Communal(int thr, int irr, int[] pt)
-	{
+
+	public Communal(int thr, int irr, int[] pt) {
 		threes = new ArrayList<ThreeBlock>();
-		for(int i = 0; i < thr; i++)
-			threes.add(new ThreeBlock());
 		irrigations = new ArrayList<OneBlock>();
-		for(int i = 0; i < irr; i++)
-			threes.add(new OneBlock(TileType.IRRIGATION));
-		for(int i = 0; i < pt.size(); i++)
-			threes.add(pt[i]);
+		palaceTiles = new ArrayList<Integer>();
+
+		for (int i = 0; i < thr; i++)
+			threes.add(new ThreeBlock());
+
+		for (int i = 0; i < irr; i++)
+			irrigations.add(new OneBlock(TileType.IRRIGATION));
+
+		for (int i = 0; i < pt.length; i++)
+			palaceTiles.add(pt[i]);
 	}
-	
-	public ThreeBlock getThreeBlock()
-	{
-		if(!threes.empty())
+
+	public ThreeBlock getThreeBlock() {
+		if (!threes.isEmpty())
 			throw new NoBlocksLeftException("No Three-Tile Blocks left.");
 		return threes.remove(0);
 	}
-	public OneBlock getIrrigationTile()
-	{
-		if(!irrigations.empty())
+
+	public OneBlock getIrrigationTile() {
+		
+		if (!irrigations.isEmpty())
 			throw new NoBlocksLeftException("No Irrigation Tiles left.");
 		return irrigations.remove(0);
 	}
-	public OneBlock getPalaceTile(int level)
-	{
-		if(palaceTiles.get(level) != 0)
-		palaceTiles.get(level) -= 1;
+
+	public OneBlock getPalaceTile(int level) {
+		//Makes no sense
+		if (palaceTiles.get(level) != 0)
+			palaceTiles.get(level) -= 1;
 		return new OneBlock(TileType.PALACE);
 	}
-	public void putBackPalaceTile(Block b)
-	{
-		if(b.getType() = TileType.PALACE)
+
+	public void putBackPalaceTile(Block b) {
+		//Makes no sense
+		if (b.getType() == TileType.PALACE)
 			palaceTiles.get(((PalaceTile) b.getTile()).getLevel()) += 1;
 	}
-	public void putBackThreeBlock(Block b)
-	{
-		if(b.getType() = TileType.THREE)
+
+	public void putBackThreeBlock(ThreeBlock b) {
+		if (b.getType() == TileType.THREE)
 			threes.add(b);
 	}
-	public void putBackIrrigationTile(Block b)
-	{
-		if(b.getType() = TileType.IRRIGATION)
-			threes.add(b);
+
+	public void putBackIrrigationTile(OneBlock b) {
+		if (b.getType() == TileType.IRRIGATION)
+			irrigations.add(b);
 	}
-	public int numThreeBlocks()
-	{
+
+	public int numThreeBlocks() {
 		return threes.size();
 	}
-	public int numIrrigationTiles()
-	{
+
+	public int numIrrigationTiles() {
 		return irrigations.size();
 	}
-	public int numPalaceTiles(int level)
-	{
+
+	public int numPalaceTiles(int level) {
 		return palaceTiles.get(level);
 	}
 }
