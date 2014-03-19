a8024dfac98e5781c8b281eb98761c8ba6cd0031
AlexBujduveanu <alexbujduveanu@gmail.com>
2014-03-19 18:51:33 -0400

    Added fields and methods for palace festivals
    ~

--- src/PhaseFestival.java
+++ src/PhaseFestival.java
@@ -1,7 +1,31 @@
-
 public class PhaseFestival {
-	ModelFacade model;
-	public PhaseFestival(ModelFacade model) {
-		this.model = model;
+	private PlayerFacade playerFacade;
+	private Sanitation sanitation;
+	private boolean isPlayerBidding;
+	private boolean lastPlayerDone;
+	private boolean isTied;
+    
+	public PhaseFestival(PlayerFacade playerFacade, Sanitation sanitation) {
+		this.playerFacade = playerFacade;
+		this.sanitation = sanitation;
 	}
+    
+	public void startFestival() {
+		playerFacade.createPalaceFestival();
+		playerFacade.startFestival(playerFacade.getPlayers());
+	}
+
+	public void currentPlayerPlaceBid(String[] cardSymbol) {
+		playerFacade.play(cardSymbol);
+		playerFacade.nextPFPlayer();
+	}
+
+	public void currentPlayerPassBid() {
+
+	}
+    
+	public void endFestival() {
+		
+	}
+    
 }
