daefcdf25d607bdc0e34e2090179b8cb5b1a49ce
AlexBujduveanu <alexbujduveanu@gmail.com>
2014-03-20 07:40:34 -0400

    Added festival phase methods
    Added festival phase methods

--- src/PhaseFestival.java
+++ src/PhaseFestival.java
@@ -15,10 +15,10 @@ public class PhaseFestival {
 		playerFacade.startFestival(playerFacade.getPlayers());
 	}
 
-	public void currentPlayerPlaceBid(String[] cardSymbol) {
+	/*public void currentPlayerPlaceBid(String[] cardSymbol) {
 		//playerFacade.play(cardSymbol);
 		playerFacade.nextPFPlayer();
-	}
+	}*/
 
 	public void currentPlayerPassBid() {
 		playerFacade.freezePlayer();
@@ -33,6 +33,62 @@ public class PhaseFestival {
 		}
 		return false;
 	}
+
+	public void playDrumCard()
+	{
+		String[] type = new String[1];
+		type[0] = "DRUM";
+		PlayPalaceCardCommand p = new PlayPalaceCardCommand(playerFacade, type);
+		p.execute();
+	}
+
+	public void playPuppetCard()
+	{
+		String[] type = new String[1];
+		type[0] = "PUPPET";
+		PlayPalaceCardCommand p = new PlayPalaceCardCommand(playerFacade, type);
+		p.execute();
+	}
+
+	public void playMaskCard()
+	{
+		String[] type = new String[1];
+		type[0] = "MASK";
+		PlayPalaceCardCommand p = new PlayPalaceCardCommand(playerFacade, type);
+		p.execute();
+	}
+
+	public void playMaskDrumCard()
+	{
+		String[] type = new String[2];
+		type[0] = "MASK";
+		type[1] = "DRUM";
+		PlayPalaceCardCommand p = new PlayPalaceCardCommand(playerFacade, type);
+		p.execute();
+	}
+
+	public void playDrumPuppetCard()
+	{
+		String[] type = new String[2];
+		type[0] = "DRUM";
+		type[1] = "PUPPET";
+		PlayPalaceCardCommand p = new PlayPalaceCardCommand(playerFacade, type);
+		p.execute();
+	}
+
+	public void playPuppetMaskCard()
+	{
+		String[] type = new String[2];
+		type[0] = "PUPPET";
+		type[1] = "MASK";
+		PlayPalaceCardCommand p = new PlayPalaceCardCommand(playerFacade, type);
+		p.execute();
+	}
+
+
+
+
+
 
 	
     
