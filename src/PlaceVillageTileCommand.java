8ebb23e94a3f6bb2e6261475fcf9af6f72cd91df
ktn <kevinnieman@gmail.com>
2014-03-19 22:47:11 -0400

    Add place village command

---
+++ src/PlaceVillageTileCommand.java
@@ -0,0 +1,46 @@
+public class PlaceVillageTileCommand implements Command {
+	private BoardFacade board;
+	private Board.Coordinates coords;
+	private PlayerFacade player;
+	private Block block;
+
+	PlaceVillageTileCommand(BoardFacade b, PlayerFacade p,
+			Board.Coordinates c) {
+		this.board = b;
+		this.coords = c;
+		this.player = p;
+	}
+
+	public void execute() {
+		// assume checks have been made
+		block = new OneBlock(TileType.VILLAGE);
+		player.placeVillage();
+		// if(board.validPlacement(coors, b)
+		board.placeBlock(coords, block);
+		this.save();
+
+	}
+
+	public void undo() {
+		board.removeBlock(coords);
+
+		player.returnVillageBlock();
+
+	}
+
+
+	public void save(){
+		CommandStack.storeCommand(this);
+	}
+
+	public void load() {
+
+	}
+
+	public String toString() {
+		StringBuilder result = new StringBuilder(100);
+		result.append(this.getClass().getName() + " " + coords.x + " "
+				+ coords.y);
+		return result.toString();
+	}
+}
